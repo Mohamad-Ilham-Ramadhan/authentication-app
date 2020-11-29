ca0d98023afd15044f92f6d5353f5af492032321
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2020-11-29 14:50:19 +0700

    updateUser action revamped to use async/await

--- src/config/redux/actions/updateUser.js
+++ src/config/redux/actions/updateUser.js
@@ -12,181 +12,128 @@ export default function updateUser({
   file,
 }) {
   return function (dispatch, getState) {
+    console.log('file =>', file);
     const user = firebase.auth().currentUser;
     const newData = { displayName, bio, phoneNumber };
+    const profileRef = storage.ref().child(`/images/profile/${user.uid}`);
 
-    dispatch(setLoadingProfileEdit(true));
-    console.log(file);
-    // mencoba mengupload ke firebase storage
-    const storageRef = storage.ref()
-    const profileRef = storageRef.child(`/images/profile/${user.uid}`)
-    
-    user
-      .updateEmail(email)
-      .then((response) => {
-        console.log("update email success");
-        database
-          .ref(`users/${user.uid}`)
-          .update({ email })
-          .then((response) => {
-            dispatch({ type, payload: { email } });
-            dispatch(setErrMsgProfileEdit({ email: "" }));
-            // reload user
-            user
-              .reload() // The best answer
-              .then(() => {
-                console.log("reauthenticate succes, and response is =>");
-                // lalu update password
-                if (password.length > 0) {
-                  user
-                    .updatePassword(password)
-                    .then(() => {
-                      console.log("Update password berhasil");
-                      database
-                        .ref(`users/${user.uid}`)
-                        .update({ password: true })
-                        .then((response) => {
-                          dispatch({ type, payload: { password: true } });
-                          dispatch(setErrMsgProfileEdit({ password: "" }));
-                          // user reauthenticate lagi untuk updateProfile()
-                          user.reload().then(() => {
-                            // photoURL 
-                            profileRef.put(file).then( snapshot => {
-                              console.log('snapshot =>', snapshot);
-                              profileRef.getDownloadURL().then(url => {
-                                console.log(url);
-                                user.updateProfile({photoURL: url}).then(() => {
-                                  database.ref(`users/${user.uid}`).update({photoURL: url}).then(() => {
-                                    console.log('update photo propil sukses!')
-                                  })
-                                })
-                              })
-                            }).catch( err => {
-                              console.log('upload photo error => ', err);
-                            })
-                            //
-                            user
-                              .updateProfile(newData)
-                              .then(() => {
-                                database
-                                  .ref(`users/${user.uid}`)
-                                  .update(newData)
-                                  .then((response) => {
-                                    console.log("update profile sukses");
-                                    dispatch({ type, payload: newData });
-                                    dispatch(
-                                      setErrMsgProfileEdit({
-                                        displayName: "",
-                                        bio: "",
-                                        phoneNumber: "",
-                                      })
-                                    );
-                                    dispatch(setLoadingProfileEdit(false));
-                                  })
-                                  .catch((err) => {
-                                    console.log("update profile gagal =>", err);
-                                    const msg = err.message;
-                                    dispatch(
-                                      setErrMsgProfileEdit({
-                                        displayName: msg,
-                                        bio: msg,
-                                        phoneNumber: msg,
-                                      })
-                                    );
-                                    dispatch(setLoadingProfileEdit(false));
-                                  });
-                              })
-                              .catch((err) => {
-                                console.log("update profile gagal =>", err);
-                                const msg = err.message;
-                                dispatch(
-                                  setErrMsgProfileEdit({
-                                    displayName: msg,
-                                    bio: msg,
-                                    phoneNumber: msg,
-                                  })
-                                );
-                                dispatch(setLoadingProfileEdit(false));
-                              });
-                          });
-                        })
-                        .catch((err) => {
-                          console.log("update password gagal =>", err);
-                          const msg = err.message;
-                          dispatch(setErrMsgProfileEdit({ password: msg }));
-                          dispatch(setLoadingProfileEdit(false));
-                        });
-                    })
-                    .catch((err) => {
-                      console.log("update password gagal =>", err);
-                      const msg = err.message;
-                      dispatch(setErrMsgProfileEdit({ password: msg }));
-                      dispatch(setLoadingProfileEdit(false));
-                    });
-                } else {
-                  // jika tidak updatePassword() maka updateProfile() tetap dieksekusi
-                  user
-                    .updateProfile(newData)
-                    .then(() => {
-                      database
-                        .ref(`users/${user.uid}`)
-                        .update(newData)
-                        .then((response) => {
-                          console.log("update profile sukses");
-                          dispatch({ type, payload: newData });
-                          dispatch(
-                            setErrMsgProfileEdit({
-                              displayName: "",
-                              bio: "",
-                              phoneNumber: "",
-                            })
-                          );
-                          dispatch(setLoadingProfileEdit(false));
-                        })
-                        .catch((err) => {
-                          console.log("update profile gagal =>", err);
-                          const msg = err.message;
-                          dispatch(
-                            setErrMsgProfileEdit({
-                              displayName: msg,
-                              bio: msg,
-                              phoneNumber: msg,
-                            })
-                          );
-                          dispatch(setLoadingProfileEdit(false));
-                        });
-                    })
-                    .catch((err) => {
-                      console.log("update profile gagal =>", err);
-                      const msg = err.message;
-                      dispatch(
-                        setErrMsgProfileEdit({
-                          displayName: msg,
-                          bio: msg,
-                          phoneNumber: msg,
-                        })
-                      );
-                    });
-                }
-              })
-              .catch((err) => {
-                console.log("reauth is fail and the error is =>", err);
-                dispatch(setLoadingProfileEdit(false));
-              });
-          })
-          .catch((err) => {
-            const msg = err.message;
-            dispatch(setErrMsgProfileEdit({ email: msg }));
-            dispatch(setLoadingProfileEdit(false));
-          });
-      })
-      .catch((err) => {
-        console.log("update email gagal =>", err);
+    async function updateProfile() {
+      if (file == null) {
+        try {
+          const data = newData;
+          const updateProfileAuth = user.updateProfile(data);
+          console.log('update profile auth SUKSES');
+          const updateProfileDatabase = database.ref(`users/${user.uid}`).update(data);
+          console.log('update profile di realtime database SUKSES!');
+          dispatch({ type, payload: data });
+          dispatch(
+            setErrMsgProfileEdit({
+              displayName: "",
+              bio: "",
+              phoneNumber: "",
+              photoURL: '',
+            })
+          );
+        } catch (err) {
+          console.log("update profile gagal =>", err);
+          const msg = err.message;
+          dispatch(
+            setErrMsgProfileEdit({
+              displayName: msg,
+              bio: msg,
+              phoneNumber: msg,
+              photoURL: msg,
+            })
+          );
+          dispatch(setLoadingProfileEdit(false));
+        }
+      } else {
+        try {
+          const snapshot = await profileRef.put(file);
+          console.log('Taruh photo profile di storage SUKSES');
+          const url = await profileRef.getDownloadURL();
+          console.log('Url didapatkan =>', url);
+          const data = { ...newData, photoURL: url };
+          const updateProfileAuth = user.updateProfile(data);
+          console.log('update profile auth SUKSES');
+          const updateProfileDatabase = database.ref(`users/${user.uid}`).update(data);
+          console.log('update profile di realtime database SUKSES!');
+          dispatch({ type, payload: data });
+          dispatch(
+            setErrMsgProfileEdit({
+              displayName: "",
+              bio: "",
+              phoneNumber: "",
+              photoURL: '',
+            })
+          );
+        } catch (err) {
+          console.log("update profile gagal =>", err);
+          const msg = err.message;
+          dispatch(
+            setErrMsgProfileEdit({
+              displayName: msg,
+              bio: msg,
+              phoneNumber: msg,
+              photoURL: msg,
+            })
+          );
+          dispatch(setLoadingProfileEdit(false));
+        }
+
+      }
+      dispatch(setLoadingProfileEdit(false));
+    }
+    // dispatch(setLoadingProfileEdit(true));
+    async function updateEmail() {
+      try {
+        const updateEmailAuth = await user.updateEmail(email);
+        console.log('update email in auth SUKES');
+        const updateEmailDatabase = await database.ref(`users/${user.uid}`).update({ email });
+        console.log('update email di realtime database SUKSES');
+        dispatch({ type, payload: { email } });
+        dispatch(setErrMsgProfileEdit({ email: '' }));
+      } catch (err) {
+        console.log('update email GAGAL');
         const msg = err.message;
         dispatch(setErrMsgProfileEdit({ email: msg }));
-        rej("email failed");
-      });
+        dispatch(setLoadingProfileEdit(false));
+      }
+      // reload user
+      try {
+        const userReload = await user.reload();
 
-    
+        console.log('reauthenticate (reload) SUKSES');
+      } catch (err) {
+        console.log('reauthenticate (reload) GAGAL');
+        dispatch(setLoadingProfileEdit(false));
+      }
+      if (password.length > 0) {
+        try {
+          const updatePasswordAuth = await user.updatePassword(password);
+          console.log('update password auth SUKSES')
+          const updatePasswordDatabase = await database.ref(`user/${user.uid}`).update({ password: true })
+          console.log('update password di database SUKSES')
+          dispatch({ type, payload: { password: true } })
+          dispatch(setErrMsgProfileEdit({ password: '' }))
+        } catch (err) {
+          console.log("update password GAGAL =>", err);
+          const msg = err.message;
+          dispatch(setErrMsgProfileEdit({ password: msg }));
+          dispatch(setLoadingProfileEdit(false));
+        }
+        // re-auth buat updateProfile();
+        try {
+          const userReload = await user.reload();
+        } catch (err) {
+          console.log('reauthenticate (reload) GAGAL');
+          dispatch(setLoadingProfileEdit(false));
+        }
+        updateProfile();
+      } else {
+        updateProfile();
+      }
+    }
+    updateEmail();
   };
 }
